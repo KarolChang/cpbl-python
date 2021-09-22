48746e037e681fc0dec622d034ad93113f89e682
KarolChang <super000999888@gmail.com>
2021-09-22 14:30:01 +0800

    add index.html & 2 routes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # 使用 flask
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,11 +9,21 @@ CORS(app)
 import json
 with open("datas/live-log-json.json", mode="r") as file:
   liveLogJson = json.load(file)
+with open("datas/scoreboard-json.json", mode="r") as file:
+  scoreboardJson = json.load(file)
 
 # 路由
 @app.route("/")
-def index(): 
+def index():
+  return render_template('index.html')
+
+@app.route("/livelog")
+def livelog(): 
   return jsonify(liveLogJson)
+
+@app.route("/scoreboard")
+def scoreboard(): 
+  return jsonify(scoreboardJson)
 
 # run server
 if __name__ == "__main__":
